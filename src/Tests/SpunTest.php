10c1bdc769e4cf9fae7e8f6540d392e34d59121c
Michael James <10671926+mchljams@users.noreply.github.com>
2017-02-16 23:49:35 -0600

    update tests for spun class

--- src/Tests/SpunTest.php
+++ src/Tests/SpunTest.php
@@ -1,33 +1,62 @@
 <?php
 
-include './src/Spun.php';
+namespace Spun;
 
 class SpunTest extends \PHPUnit_Framework_TestCase
 {
 
-	protected $str;
+  public function setUp()
+  {
+    $this->one_group = "This is a string that {includes|contains|holds} choices you can spin.";
+    $this->two_groups = "This is a {sentence|string} that {includes|contains|holds} choices you can spin.";
+  }
 
+  public function tearDown()
+  {
 
-	public function setUp()
-	{
-		$this->str = "This is a string that {includes|contains|holds} choices you can spin."; 
-	}
+  }
 
-  	public function tearDown()
-  	{
+  public function testGetStrReturnsString()
+  {
+    $spun = new Spun($this->one_group);
+    $this->assertInternalType('string', $spun->getStr());
+  }
 
-  	}
-	
-	public function testTrueIsTrue()
-	{
-	    $foo = true;
-	    $this->assertTrue($foo);
-	}
+  public function testGetSequenceReturnsArray()
+  {
+    $spun = new Spun($this->one_group);
+    $this->assertInternalType('array', $spun->getSequence());
+  }
 
-	public function testRemoveAnchorsReturnsString()
-	{
-		$spun = new Spun;
-		$str = $spun->removeAnchors('{Test String Here}');
-		$this->assertInternalType('string', $str);
-	}
+  public function testCombinationsReturnsInt()
+  {
+    $spun = new Spun($this->one_group);
+    $this->assertInternalType('int', $spun->combinations());
+  }
+
+  public function testCombinationsReturnsCorrectTotalNoGroups()
+  {
+    $spun = new Spun('String with no spintax');
+    $this->assertEquals($spun->combinations(), 1);
+  }
+
+  public function testCombinationsReturnsCorrectTotalForOneGroup()
+  {
+    $spun = new Spun($this->one_group);
+    $this->assertEquals($spun->combinations(), 3);
+  }
+
+  public function testCombinationsReturnsCorrectTotalForTwoGroups()
+  {
+    $spun = new Spun($this->two_groups);
+    $this->assertEquals($spun->combinations(), 6);
+  }
+
+  public function testFingerprintReturnsObject()
+  {
+    $spun = new Spun($this->one_group);
+    $this->assertInternalType('object', $spun->fingerprint());
+  }
+
+  //left off at fingerprint method
 }
