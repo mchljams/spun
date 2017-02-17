a22bcb30660757c725ddaff8a1937823d3f68b6d
Mike Pisula <mike@harpgraphics.com>
2017-02-17 15:00:25 -0600

    add fingerprint test

---
+++ src/Tests/FingerprintTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Mchljams\Spun;
+
+class FingerprintTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+    }
+
+    public function tearDown()
+    {
+    }
+}
